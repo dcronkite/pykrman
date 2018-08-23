4a6454d93a2c0421c6f0fbfce3df3a8625f9bccd
dcronkite <dcronkite@gmail.com>
2018-08-23 12:49:50 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+Python Intelligent Character Recognition Manager
+
+# About #
+Manage ICR/OCR on a variety of inputs, primarily tiff and scanned pdfs.
+
+# License #
+MIT: https://dcronkite.mit-license.org/
---
+++ setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+import setuptools
+
+setup(name='pykrman',
+      version='0.0.1',
+      description='Python Intelligent Character Recognition Manager',
+      url='https://bitbucket.org/dcronkite/pykrman',
+      author='dcronkite',
+      author_email='dcronkite@gmail.com',
+      license='MIT',
+      classifiers=[  # from https://pypi.python.org/pypi?%3Aaction=list_classifiers
+          'Development Status :: 3 - Alpha',
+          'Programming Language :: Python :: 3 :: Only',
+      ],
+      entry_points={
+          'console_scripts':
+              [
+              ]
+      },
+      install_requires=[],
+      package_dir={'': 'src'},
+      packages=setuptools.find_packages('src'),
+      zip_safe=False
+      )
